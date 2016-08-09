155deae4c91be111a3219a9e7d0521c8d837b2ff
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-09 16:01:28 -0700

    Create gameEngine.h

---
+++ gameEngine.h
@@ -0,0 +1,26 @@
+#pragma once
+class gameEngine
+{
+public:
+	gameEngine();
+	~gameEngine();
+	
+//setters and getters	
+	void setNumberOfDice(int numDice);
+	int getNumberOfDice();
+	
+	void setGameState(int state);
+	int getGameState();
+
+	bool numbDiceCheck(int diNum);
+
+	dice *startGame(int diceNum);
+private:
+	int gameState;
+	//gameState can be Waiting(0) or Playing(1)
+	int numberOfDice;
+	//number of dice can be between 1 and 5
+	
+
+};
+
