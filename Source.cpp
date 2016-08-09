aa8ae5ae27fd3439a7f3c427788f93457ca1bdb2
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-09 16:00:46 -0700

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+//#include "dice.h"
+#include <windows.h>
+#include "gameEngine.h"
+
+using namespace std;
+
+
+
+int main() {
+	gameEngine Game;
+	/*dice di;
+	dice dic(100);
+	int i;*/
+	//SetColor(RED);
+	//cout << "please enter poop";
+	//cin >> i;
+	//SetColor(BLUE);
+	//cout << "hi hans your number is" << i << endl;
+	//int diceV = di.getDiceValue();
+	//SetColor(GREEN);
+	//cout << "dice value is "  << diceV << endl;
+	//int diceM = di.getMaxValue();
+	//SetColor(GRAY);
+	//cout << "max dice is " << diceM << endl;
+
+	//int ddiceV = dic.getDiceValue();
+	//SetColor(TEAL);
+	//cout << "dice value is " << ddiceV << endl;
+	//int ddiceM = dic.getMaxValue();
+	//SetColor(PINK);
+	//cout << "max dice is " << ddiceM << endl;
+
+
+	return 0;
+
+}
