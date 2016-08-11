443a299e1c7b0877358988ed6dc92258a80a3837
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-11 16:20:03 -0700

    Create GameStateManager.h

--- header/GameStateManager.h
+++ header/GameStateManager.h
@@ -6,6 +6,13 @@ public:
 	GameStateManager();
 	~GameStateManager();
 
+	//Setters and getters :D
+	void setGameState(enum GameStates a);
+	enum getGameState();
+
+
+
+
 private:
 	static enum class GameStates {START_MENU, GAME_INIT, ROLL_DICE, COINFLIP,TURN, CALL, RESULT, RETRY, GAMEOVER };
 	//StartMenu: Displays starting prompt to ask the user number dice;
@@ -17,4 +24,7 @@ private:
 	//Result:Determines who loses a Dice, Who wins
 	//Retry: prompts to retry
 	//GameOver: prompts Gameover ROll credits and leave
+
+	GameStates gameStates;
 };
+
