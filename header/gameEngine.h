e3abb729f3a76c3e5fea6083c9c3a44ba6bf25f2
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-11 10:22:44 -0700

    Added in jeff's vector

--- header/gameEngine.h
+++ header/gameEngine.h
@@ -1,9 +1,16 @@
 #pragma once
-class gameEngine
+#include "dice.h"
+#include <vector>
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+class GameEngine
 {
 public:
-	gameEngine();
-	~gameEngine();
+	GameEngine();
+	~GameEngine();
 	
 //setters and getters	
 	void setNumberOfDice(int numDice);
@@ -14,7 +21,8 @@ public:
 
 	bool numbDiceCheck(int diNum);
 
-	//dice *startGame(int diceNum);
+	vector<Dice*> startGame(int diceNum);//ty jeff
+
 private:
 	int gameState;
 	//gameState can be Waiting(0) or Playing(1)
