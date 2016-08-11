b5167b6c2823dc690775a3c5f53956dcf0f7bc1e
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-11 16:20:40 -0700

    Update GameStateManager.cpp

--- source/GameStateManager.cpp
+++ source/GameStateManager.cpp
@@ -10,4 +10,16 @@ GameStateManager::GameStateManager()
 
 GameStateManager::~GameStateManager()
 {
+	GameStates gameState = GameStates::START_MENU;
 }
+
+void GameStateManager::setGameState(enum GameStates a)
+{
+	GameStates gameStates = a;
+}
+
+enum GameStateManager::getGameState()
+{
+	return GameStates.gameState;
+}
+
