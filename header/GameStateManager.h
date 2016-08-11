cff5de913a2d5b1bccf6454a24485bae4335e8c5
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-11 15:30:58 -0700

    Update GameStateManager.h

--- header/GameStateManager.h
+++ header/GameStateManager.h
@@ -1,4 +1,5 @@
 #pragma once
+
 class GameStateManager
 {
 public:
@@ -6,7 +7,7 @@ public:
 	~GameStateManager();
 
 private:
-	enum class GameStates {START_MENU, GAME_INIT, ROLL_DICE, COINFLIP,TURN, CALL, RESULT, RETRY, GAMEOVER };
+	static enum class GameStates {START_MENU, GAME_INIT, ROLL_DICE, COINFLIP,TURN, CALL, RESULT, RETRY, GAMEOVER };
 	//StartMenu: Displays starting prompt to ask the user number dice;
 	//GameInit: Starts putting the Information from StartMenu into GameEngine;
 	//RollDice: Rolls the dice
@@ -17,4 +18,3 @@ private:
 	//Retry: prompts to retry
 	//GameOver: prompts Gameover ROll credits and leave
 };
-
