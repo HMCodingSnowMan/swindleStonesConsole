121faf7c8ef5d0da2574ec849e4d05cfa71f8277
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-12 13:45:55 -0700

    Update dice.cpp

--- source/dice.cpp
+++ source/dice.cpp
@@ -1,88 +1,51 @@
+#include <iostream>
+#include "dice.h"
+#include <windows.h>
 #include "gameEngine.h"
+#include "Coin.h"
+
+using namespace std;
 
 
 
-
-
-
-
-HANDLE hCon;
-
-enum Color { DARKBLUE = 1, DARKGREEN, DARKTEAL, DARKRED, DARKPINK, DARKYELLOW, GRAY, DARKGRAY, BLUE, GREEN, TEAL, RED, PINK, YELLOW, WHITE };
-
-void SetColor(Color c) {
-	if (hCon == NULL)
-		hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hCon, c);
-}
-
-
-GameEngine::GameEngine() //prompts user for menu and stuff
-{
-	setGameState(0);
-	cout << getGameState() << endl;
-	SetColor(DARKBLUE);
+int main() {
+	//GameEngine Game;
 	
-	cout << "Welcome to the SwindleStones! I'm gonna take your money!" << endl;
-	SetColor(DARKGREEN);
-	
-	cout << "We can have a quick game with 2 dice, a long game with 5 dice, or anything inbetween" << endl;
-	cin >> numberOfDice;
-	while (numbDiceCheck(getNumberOfDice()) == false) {
-		cout << "We do not play with that many dice. Try again. How many dice would you like to play with?" << endl;
-		cin >> numberOfDice;
-	}
-	cout << "excellent lets play the game with " << getNumberOfDice() << " dice" << endl;
-	setGameState(1);
-	cout << getGameState() << endl << flush;
-}
+	//GameStateManager gsm;
+	//gsm.setGameState(GameStateManager::GameStates::GAME_INIT);
+	//cout << gsm.getGameState() << endl;
+	//gsm.setGameState(GameStateManager::GameStates::ROLL_DICE);
+	//cout << gsm.getGameState() << endl;
+	//gsm.setGameState(GameStateManager::GameStates::COINFLIP);
+	//cout << gsm.getGameState() << endl;
+	//gsm.setGameState(GameStateManager::GameStates::GAMEOVER);
+	//cout << gsm.getGameState() << endl;
 
+	//Coin cn;
+	//
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
+	//cn.coinFlip();
+	//cout << "the coin value is" << cn.getCoinValue() << endl;
 
-GameEngine::~GameEngine()
-{
-}
-
-void GameEngine::setNumberOfDice(int numDice)
-{
-	numberOfDice = numDice;
-}
-
-int GameEngine::getNumberOfDice()
-{
-	return numberOfDice;
-}
-
-void GameEngine::setGameState(int state)
-{
-	gameState = state;
-}
-
-int GameEngine::getGameState()
-{
-	return gameState;
-}
-
-bool GameEngine::numbDiceCheck(int diNum)
-{
-	if ((diNum >= 2) && (diNum <= 5)) {
-		return true;
-	}
-	else
-		return false;
-}
-
-vector<Dice*> GameEngine::startGame(int diceNum)
-{
-	cout << "start the game!" << endl;
-
-	vector<Dice*> diceGame;
-	diceGame.reserve(diceNum);
-
-	for (int i = 0; i < diceNum; i++) {
-		Dice *dice = new Dice(4);
-		diceGame.push_back(dice);
-	}
-
-	return diceGame;
+	Dice di;
+	cout << di.getDiceValue() << endl;
+	cout << di.getMaxValue() << endl;
+	return 0;
 
 }
