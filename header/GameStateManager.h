22ff5da358ec9339553aaa4c7c6e329e03ca9d2e
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-12 13:47:56 -0700

    Update GameStateManager.h

--- header/GameStateManager.h
+++ header/GameStateManager.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+
 
 class GameStateManager
 {
@@ -6,15 +11,7 @@ public:
 	GameStateManager();
 	~GameStateManager();
 
-	//Setters and getters :D
-	void setGameState(enum GameStates a);
-	enum getGameState();
-
-
-
-
-private:
-	static enum class GameStates {START_MENU, GAME_INIT, ROLL_DICE, COINFLIP,TURN, CALL, RESULT, RETRY, GAMEOVER };
+	enum class GameStates { START_MENU, GAME_INIT, ROLL_DICE, COINFLIP, TURN, CALL, RESULT, RETRY, GAMEOVER };
 	//StartMenu: Displays starting prompt to ask the user number dice;
 	//GameInit: Starts putting the Information from StartMenu into GameEngine;
 	//RollDice: Rolls the dice
@@ -24,7 +21,22 @@ private:
 	//Result:Determines who loses a Dice, Who wins
 	//Retry: prompts to retry
 	//GameOver: prompts Gameover ROll credits and leave
+	
+	
+	//Setters and getters :D
+	//void setGameState(GameStates a);
 
-	GameStates gameStates;
+	string getGameStateString();
+
+	void setGameState(GameStates a);
+
+	GameStates getGameState();
+
+
+
+private:
+
+
+	GameStates gameState;
 };
 
