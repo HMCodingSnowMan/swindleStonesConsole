cb33f32a38f3676f80b62f679f474bd85ace7faa
Hans Marasigan <hansmarasigan@gmail.com>
2016-08-12 13:48:18 -0700

    Update gameEngine.h

--- header/gameEngine.h
+++ header/gameEngine.h
@@ -3,12 +3,16 @@
 #include <vector>
 #include <windows.h>
 #include <iostream>
+#include "GameStateManager.h"
+#include <string>
 
 using namespace std;
 
 class GameEngine
 {
 public:
+	GameStateManager gsm;
+
 	GameEngine();
 	~GameEngine();
 	
@@ -16,18 +20,21 @@ public:
 	void setNumberOfDice(int numDice);
 	int getNumberOfDice();
 	
-	void setGameState(int state);
-	int getGameState();
+	//void setGameState();
+	//string getGameState();
+
+
 
 	bool numbDiceCheck(int diNum);
 
-	vector<Dice*> startGame(int diceNum);//ty jeff
+	void rollTheDice(vector<Dice>&);//ty jeff
 
 private:
-	int gameState;
+	//int gameState;
 	//gameState can be Waiting(0) or Playing(1)
 	int numberOfDice;
 	//number of dice can be between 1 and 5
+	string gState;
 	
 
 };
